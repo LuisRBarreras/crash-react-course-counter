bbae0026fb4a13a2acfd8d18b16f60bbbeb827a7
Luis Barreras <luis.barreras@unosquare.com>
2019-09-11 05:25:57 -0700

    Add counter functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
+    state = {
+        count: 0,
+        address: {
+            street: ''
+        }
+    };
+    
+
+    render() {
+        return (
+            <div>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
- 
+
 export default Counter;
